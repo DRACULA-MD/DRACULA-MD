beefc64f1be9fcc072f5281f43dd27c0a50a4420
DRACULA-MD <deslasspharouk@gmail.com>
2025-04-11 20:31:42 +0000

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -658,14 +658,14 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         await conn.sendMessage(
             from,
             {
-                image: { url: `https://files.catbox.moe/7zfdcq.jpg` },
+                image: { url: `https://files.catbox.moe/sauy49.jpg` },
                 caption: dec,
                 contextInfo: {
                     mentionedJid: [m.sender],
                     forwardingScore: 999,
                     isForwarded: true,
                     forwardedNewsletterMessageInfo: {
-                        newsletterJid: '120363354023106228@newsletter',
+                        newsletterJid: '',
                         newsletterName: 'JawadTechX',
                         serverMessageId: 143
                     }
@@ -676,7 +676,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
 
         // Send audio
         await conn.sendMessage(from, {
-            audio: { url: 'https://github.com/XdTechPro/KHAN-DATA/raw/refs/heads/main/autovoice/menunew.m4a' },
+            audio: { url: '' },
             mimetype: 'audio/mp4',
             ptt: true
         }, { quoted: mek });
